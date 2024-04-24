afefa9dc84eb8b0ad24c0e6d2299c47ef23c67be
maliahgp <maliahgp@ad.unc.edu>
2024-04-24 18:19:54 -0400

    database is setup the same way as A05

---
+++ database/setup_db.mjs
@@ -0,0 +1,8 @@
+import {db} from './db.mjs'
+
+//creating the tasks database
+
+await db.run(`CREATE TABLE tasks (id INTEGER PRIMARY KEY, title TEXT, description TEXT, 
+                due_date TEXT NOT NULL CHECK (due_date LIKE '____-__-__'),
+                complete BOOLEAN NOT NULL CHECK (complete IN (0, 1)),
+                category TEXT NOT NULL CHECK (category IN ('home', 'work', 'school'))`)
