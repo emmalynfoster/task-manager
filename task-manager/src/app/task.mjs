8635dc4b686b53877fa14ee3cecc5debafaeefad
Oscar Hernandez <ojhern24@ad.unc.edu>
2024-04-26 23:49:12 -0400

    Checks if tasks exist and update task should now commit to db

--- task-manager/src/app/task.mjs
+++ task-manager/src/app/task.mjs
@@ -19,9 +19,9 @@ export class Task {
     }
 
     static categories = {
-        HOME: "Home",
-        WORK: "Work",
-        SCHOOL: "School"
+        HOME: "HOME",
+        WORK: "WORK",
+        SCHOOL: "SCHOOL"
       };
 
     static async create(data){
@@ -71,71 +71,50 @@ export class Task {
                 return new Task(row.id, row.title, row.description, row.due_date, row.complete, row.category);
             }
         } 
-        
         catch (e) { return null; }
     }
 
-    /*  This is not complete, as the function works right now it can result in unwanted behavior where if any field 
-        can be updated but if another field fails, the valid ones will be updated in the table but the invalid ones 
-        stay unchanged when ideally you would want all or nothing. The setters method themselves work but are missing
-        validation.
-        */
-       // One way to fix this is by starting a transaction on the db which will only commit the changes if all the queriers go through
-
     async updateTask(data){
-        //Begin the transaction
+        // Begin the transaction
         await db.run("BEGIN TRANSACTION");
 
         try {
-            //Check that data is an Object]
-            console.log("hello")
-            console.log(data)
+            // Check that data is an Object 
             if ((data !== undefined) && (data instanceof Object)) {
-                console.log("hello")
-                /*We are now checking whether the person provided no value or the same value as before the update 
-                If they did then we do not want that query to be in the transaction becasue it wont go through
-                No need to check the type of the value becasue if it is the wrong type, the query will fail and the none 
-                of the changes in the transaction will be commited*/
-                
-                
                 
                 if(data.title !== undefined && data.title !== this.#title) {
-                    await db.run('UPDATE tasks SET title = ? WHERE id = ?', data.title, this.id);
+                    await db.run('UPDATE tasks SET title = ? WHERE id = ?', data.title, this.#id);
                     this.setTitle(data.title);
                 } 
                 
                 if(data.description !== undefined && data.description !== this.#description) {
-                    await db.run('UPDATE tasks SET description = ? WHERE id = ?', data.description, this.id);
-                    this.setDescription(data.desciption);
+                    await db.run('UPDATE tasks SET description = ? WHERE id = ?', data.description, this.#id);
+                    this.setDescription(data.description);
                 }
 
                 if(data.due_date !== undefined && data.due_date !== this.#due_date) {
-                    await db.run('UPDATE tasks SET due_date = ? WHERE id = ?', data.due_date, this.id);
+                    await db.run('UPDATE tasks SET due_date = ? WHERE id = ?', data.due_date, this.#id);
                     this.setDueDate(data.due_date);
                 }
 
                 if(data.category !== undefined && data.category !== this.#category) {
-                    await db.run('UPDATE tasks SET category = ? WHERE id = ?', data.category, this.id);
+                    await db.run('UPDATE tasks SET category = ? WHERE id = ?', data.category, this.#id);
                     this.setCategory(data.category);
                 }
 
                 if(data.completed !== undefined && data.completed !== this.#completed) {
-                    await db.run('UPDATE tasks SET complete = ? WHERE id = ?', data.complete, this.id);
+                    await db.run('UPDATE tasks SET complete = ? WHERE id = ?', data.completed, this.#id);
                     this.setCompleted(data.completed);
                 }
-                // await db.run('UPDATE tasks SET title = ?,description=?, due_date=?, completed=?, category=?  WHERE id = ?', data.title, data.description, data.due_date, data.completed, data.category, this.id);
-
-
-
-            } else {
-                //If data is not an Object, return
-                return;
+            } 
+            
+            else { 
+                db.run("ROLLBACK");
+                return null;
             }
-
-            //If there are no errors, commit all changes made during transaction and update the values in the object
-            await db.run("COMMIT"); //NOT COMMITING???? 'this' changes but they're not actually reflected in the db
-            return this
-
+    
+            await db.run("COMMIT");
+            return this;
         }
 
         catch (e) {
@@ -143,36 +122,8 @@ export class Task {
             db.run("ROLLBACK");
             return null;
         }
-
-    }         
-        
-            // Ensure that new data is not undefined, is the correct type and does not match its current value to update
-    //         if (data.title !== undefined && (typeof data.title == 'string') && data.title !== this.#title){
-    //             this.setTitle(data.title);
-    //         }
-
-    //         if (data.description !== undefined && (typeof data.description == 'string') && data.description !== this.#description){
-    //             this.setDescription(data.description);
-    //         }
-
-    //         if (data.due_date !== undefined && (typeof data.due_date == 'string') && data.due_date !== this.#due_date){
-    //             this.setDueDate(data.due_date);
-    //         }
-
-    //         if (data.category !== undefined && (typeof data.category == 'string') && data.category !== this.#category && data.category in categories){
-    //             this.setCategory(data.category);
-    //         }
-
-    //         if (data.completed !== undefined && Number.isInteger (data.completed) && data.completed !== this.#completed){
-    //             this.setCompleted(data.completed);
-    //         }
-
-    //     }
-
-    //     return;
-    // }
-
-
+    }       
+    
     static async deleteTask(id){
         try {
             await db.run('Delete from tasks where id = ?', id);
