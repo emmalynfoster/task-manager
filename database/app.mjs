e5bcbea3d81f3375175c21092c85bc192a8cddc0
ojhern24 <89327851+ojhern24@users.noreply.github.com>
2024-04-27 11:26:13 -0400

    Checks if tasks exist and update task should now commit to db (#20)
    * Checks if tasks exist and update task should now commit to db
    
    * Delete package-lock.json

--- database/app.mjs
+++ database/app.mjs
@@ -10,77 +10,65 @@ app.use(bodyParser.json())
 
 // TASK API
 
-// retrieve array of all tasks
+// Retrieve array of all tasks
+
 app.get('/tasks', async (req, res) => {
-    let result = await Task.getAllTasks()
-    res.json(result.map((task)=> task.json()))
+    let result = await Task.getAllTasks();
+    res.json(result.map((task)=> task.json()));
 })
 
 app.get('/tasks/:id', async (req, res) => {
-    let result = await Task.getTask(req.params.id)
-    res.json(result.json())
+    let result = await Task.getTask(req.params.id);
+    if (result == null){ res.status(404).send("Task not found"); }
+    else { res.json(result.json()); }
 })
 
 app.post('/tasks', async (req, res) => {
     let task = await Task.create(req.body);
-
-    if(!task) {
-        res.status(400).send("bad request");
-        return;
-    }
-    
-    res.status(201).json(task.json())
-
+    if(!task) { res.status(400).send("Bad request"); }
+    else{ res.status(201).json(task.json()); }
 })
 
 app.put('/tasks/:id', async (req, res) => {
     let task = await Task.getTask(req.params.id);
-    if (!task) {
-        res.status(404).send("Task not found");
-        return
+    if (task !== null) {
+        if (req.body !== undefined){
+            let new_task = await task.updateTask(req.body);
+            res.status(200).json(new_task.json());
     }
-    console.log(task)
-    let new_task = await task.updateTask(req.body)
-    res.status(200).json(new_task.json())
+    else { res.status(400).send("Bad request"); }
+}
 })
 
 app.delete('/tasks/:id', async (req, res) => {
-    //implement
     let deleted = await Task.deleteTask(req.params.id)
-    if(deleted){
-        res.status(200).send("task deleted")
-        return
-    }
-    res.status(400).send("delete failed")
-    
+    if (deleted){ res.status(200).send("Task deleted"); }
+    else{ res.status(400).send("Delete failed"); }
 })
 
 
 // REMINDER API
 app.get('/reminders', async (req, res) => {
-    let result = await Reminder.getAllReminders()
-    res.json(result.map((reminder)=> reminder.json()))
+    let result = await Reminder.getAllReminders();
+    res.json(result.map((reminder)=> reminder.json()));
 })
 
 app.get('/reminders/:id', async (req, res) => {
-    let result = await Reminder.getReminder(req.params.id)
-    res.json(result.json())
+    let result = await Reminder.getReminder(req.params.id);
+    if (result == null){ res.status(404).send("Task not found"); }
+    else { res.json(result.json()); }
 })
 
 app.post('/reminders', async (req, res) => {
     let reminder = await Reminder.create(req.body);
-
-    if(!reminder) {
-        res.status(400).send("bad request");
-        return;
-    }
-    
-    res.status(201).json(reminder.json())
-
+    if(!reminder) { res.status(400).send("Bad request"); }
+    else { res.status(201).json(reminder.json()); }
 })
 
 app.delete('/reminders/:id', async (req, res) => {
-    //implement
+    let deleted = await Reminder.deleteReminder(req.params.id)
+    if(deleted){ res.status(200).send("Reminder deleted"); }
+    else{ res.status(400).send("Delete failed"); }
 })
 
 app.listen(port, () => {
