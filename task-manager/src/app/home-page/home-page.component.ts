707af7a61965949ed0dead718e9d08fe27c1eb5d
Oscar Hernandez <ojhern24@ad.unc.edu>
2024-04-30 17:32:27 -0400

    3rd party api fetch button

--- task-manager/src/app/home-page/home-page.component.ts
+++ task-manager/src/app/home-page/home-page.component.ts
@@ -16,17 +16,47 @@ export class HomePageComponent {
   public category1: number[];
   public category2: number[];
   public category3: number[];
+  public suggestion: string;
 
 
   constructor(private router: Router){
     this.category1 = [1,2,3,4,5,6];
     this.category2 = [1,2,3];
     this.category3 = [1,2,3];
+    this.suggestion = '';
   }
 
   navigateToNewTask(){
     this.router.navigate(['edit']);
   }
+  
+
+  async getActivity() {
+    try {
+        const response = await fetch("https://www.boredapi.com/api/activity");
+        //const response = await fetch("https://www.boredapi.com/api/activity?type=recreational");
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+        console.log(data.activity);
+        return data.activity;
+    }
+    catch (error) {
+        console.error("Error fetching data:", error);
+        return null;
+    }
+  }
+
+
+  handleClick(){
+    this.getActivity().then(data => {
+      this.suggestion = data + '.';
+    }).catch(error => {
+      this.suggestion = "Sorry, a suggestion could not be retrieved. Try again later.";
+      console.error("Error fetching text data:", error);
+    });
+}
 
 }
 
