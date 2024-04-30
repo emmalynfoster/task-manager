9f0a55d0a8e5b1aee04020378d98731a4b2edde3
Mary Kane <marycar@ad.unc.edu>
2024-04-30 16:22:09 -0400

    user service progress

--- task-manager/src/app/services/user.service.ts
+++ task-manager/src/app/services/user.service.ts
@@ -8,5 +8,18 @@ export class UserService {
 
   constructor(private webRequestService: WebRequestService) { }
 
+
+  getUserByName(name: string) {
+    return this.webRequestService.get('users/name')
+  }
+
+  getPreferenceByName(name: string) {
+    return this.webRequestService.get('preference/name')
+  }
+  
+  createNewUser(name: string, dark_mode: number){
+    return this.webRequestService.post('users', {name, dark_mode})
+  }
   //Use webRequestService to implement methods similar to the ones in task.service.ts
 }
+
