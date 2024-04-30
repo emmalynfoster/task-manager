f0625e635357945d092e6da14efa722ecb29bb44
Oscar Hernandez <ojhern24@ad.unc.edu>
2024-04-29 23:30:36 -0400

    removed ability for fields to be optional

--- task-manager/src/app/services/task.service.ts
+++ task-manager/src/app/services/task.service.ts
@@ -28,11 +28,11 @@ export class TaskService {
 
   //use webRequestService.put method to update task **Not complete
   //Also not sure how to implement beacsue we need the task id 
-  updateTask(title ?: string, description ?: string, due_date ?: string, completed ?: number, category ?: string) {
+  updateTask(title: string, description: string, due_date: string, completed: number, category: string) {
     return this.webRequestService.put('tasks', {title, description, due_date, completed, category});
   }
 
-  deleteTask(id ?: number) {
+  deleteTask(id: number) {
     return this.webRequestService.delete('tasks/' + id);
   }
 }
