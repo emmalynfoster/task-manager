1df7728e99bf625b105853f86d899a4c63e94476
ojhern24 <89327851+ojhern24@users.noreply.github.com>
2024-04-30 17:40:44 -0400

    3rd party api fetch button (#39)
    * Create Add Task Button, New Login Page, Dark Mode Frontend Functionality (#33)
    
    * Dark mode service and new login page and add task button
    
    * change background color
    
    * 3rd party api fetch button
    
    ---------
    
    Co-authored-by: emmalynfoster <111466810+emmalynfoster@users.noreply.github.com>
    Co-authored-by: Maliah Petefish <111462499+maliahgp@users.noreply.github.com>

--- task-manager/src/app/home-page/home-page.component.ts
+++ task-manager/src/app/home-page/home-page.component.ts
@@ -23,7 +23,7 @@ export class HomePageComponent {
     this.taskService.getAllWork().subscribe({
       next: (tasks) => (this.workTasks = tasks)
     });
-
+    
     this.taskService.getAllHome().subscribe({
       next: (tasks) => (this.homeTasks = tasks)
     }); 
@@ -35,11 +35,38 @@ export class HomePageComponent {
   }
 
   constructor(private router: Router, private taskService: TaskService){
-   
   }
 
   navigateToNewTask(){
     this.router.navigate(['edit/new']);
   }
 
+  async getActivity() {
+    try {
+        const response = await fetch("https://www.boredapi.com/api/activity");
+        //const response = await fetch("https://www.boredapi.com/api/activity?type=recreational");
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+        console.log(data.activity);
+        return data.activity;
+    }
+    catch (error) {
+        console.error("Error fetching data:", error);
+        return null;
+    }
+  }
+
+
+  handleClick(){
+    this.getActivity().then(data => {
+      this.suggestion = data + '.';
+    }).catch(error => {
+      this.suggestion = "Sorry, a suggestion could not be retrieved. Try again later.";
+      console.error("Error fetching text data:", error);
+    });
+}
+
+}
 }
